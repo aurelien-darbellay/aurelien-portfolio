ffd2d1eb98e527064222e29289aba0a3d0110b15
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-11-16 17:26:33 +0100

    minor layout changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-export default function Header(){
+import React from "react";
+export default function Header() {
   return (
     <header>
       <div className="container nav">
@@ -7,11 +7,16 @@ export default function Header(){
           <div className="logo">AD</div>
           <span>Aurélien Darbellay</span>
         </a>
-        <nav>
+        <nav className="noWrapFlex">
           <a href="#portfolio">Portfolio</a>
-          <a href="mailto:aurelien.darbellay@gmail.com" className="button ghost">Contact</a>
+          <a
+            href="mailto:aurelien.darbellay@gmail.com"
+            className="button ghost"
+          >
+            Contact
+          </a>
         </nav>
       </div>
     </header>
-  )
+  );
 }
